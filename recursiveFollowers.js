de04c88828a2674f18d5b858d18772a75205ac10
Thomas Simeroth <thomassimeroth@gmail.com>
2019-01-14 13:57:44 -0600

    Preven unnecessary API calls

--- recursiveFollowers.js
+++ recursiveFollowers.js
@@ -1,15 +1,16 @@
 findFollowers = require('./findFollowers');
 
 recursiveFollowers = async function(username, depth = 3) {
+    if(depth <= 0)  {
+        return {login: username, followers: shortList};
+    }
     let followerList = await findFollowers(username);
     let foundUsers = 0;
     let shortList = [];
     let userFollowers = [];
     while(foundUsers < 3 && followerList.length > 0) {
         user = followerList.pop();
-        if(depth > 0) {
-            userFollowers = await recursiveFollowers(user.login, depth - 1);
-        }
+        userFollowers = await recursiveFollowers(user.login, depth - 1);
         shortList = shortList.concat([userFollowers]);
         foundUsers += 1;
     }
@@ -19,5 +20,4 @@ recursiveFollowers = async function(username, depth = 3) {
     }
     return result
 }
-
 module.exports = recursiveFollowers;
