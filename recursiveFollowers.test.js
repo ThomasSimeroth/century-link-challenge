d4b727f4ee1722a01cbb1e8997fcf0ca5d0872bd
Thomas Simeroth <thomassimeroth@gmail.com>
2019-01-14 12:11:29 -0600

    Make sure it works for small follower lists

--- recursiveFollowers.test.js
+++ recursiveFollowers.test.js
@@ -18,9 +18,11 @@ beforeAll(() => {
 
     findFollowers.mockImplementation((username) => {
         if(username === "abe") {
-            return [{"login": "bob"}, {"login": "chris"}, {"login": "dave"}];
+            return [{"login": "bill"}, {"login": "chris"}, {"login": "dave"}];
         } else if(username === "bob") {
             return [{"login": "ed"}, {"login": "frank"}, {"login": "graham"}, {"login": "harry"}];
+        } else if(username === "carl") {
+            return [{"login": "ed"}];
         } else {
             return [];
         }
@@ -30,11 +32,17 @@ beforeAll(() => {
 test('works one level deep', async () => {
     const data = await recursiveFollowers('abe');
     expect(data.login).toEqual("abe");
-    expect(data.followers.sort(loginSort)).toEqual([{"login": "bob"}, {"login": "chris"}, {"login": "dave"}].sort(loginSort))
+    expect(data.followers.sort(loginSort)).toEqual([{"login": "bill"}, {"login": "chris"}, {"login": "dave"}].sort(loginSort))
 });
 
 test('returns only 3 followers', async () => {
     const data = await recursiveFollowers('bob');
     expect(data.login).toEqual("bob");
     expect(data.followers.length).toBe(3);
+});
+
+test('works for less than 3 followers', async () => {
+    const data = await recursiveFollowers('carl');
+    expect(data.login).toEqual("carl");
+    expect(data.followers).toEqual([{"login": "ed"}]);
 })
